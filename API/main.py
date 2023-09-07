684cecf8f51ebfee6705f4810ecc9db265ce3521
uriellmendezz <urielemilianomendez@gmail.com>
2023-09-07 00:38:35 -0300

    adding endpoints to main.py

--- API/main.py
+++ API/main.py
@@ -6,8 +6,9 @@ from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse
-'''from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles'''
+import pandas as pd
+#from fastapi.templating import Jinja2Templates
+#from fastapi.staticfiles import StaticFiles
 
 # Configura las credenciales de Google Cloud
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_credentials.json'
@@ -18,22 +19,13 @@ dataset_name = 'new_york_transport_project'
 # Crea una instancia de FastAPI
 app = FastAPI()
 
-'''
-- Top 5 barrios con mas viajes en los ultimos 10 dias (ordenado de mayor a menor)
-- Top 5 barrios con menos viajes en los ultimos 10 dias (ordenado de mayor a menor)
-- Predicción de concentraciones de CO para la ciudad de Manhattan en una determinada fecha futura
-- Consultar por un barrio en particular y ver que porcentaje de viajes le corresponde a cada tipo de transporte
-- Prediccion de cantidad de viajes para un tipo de transporte en especifico
-
-'''
-
 # Define la ruta de inicio que servirá la página HTML
 @app.get("/")
 def read_root():
     message = '''
         Welcome to the New York Taxi Project API!
 
-        You request for the following endpoints:
+        You can request for the following endpoints:
             - Top 5 barrios con mas viajes en los ultimos 10 dias (ordenado de mayor a menor)
             - Top 5 barrios con menos viajes en los ultimos 10 dias (ordenado de mayor a menor)
             - Predicción de concentraciones de CO para la ciudad de Manhattan en una determinada fecha futura
@@ -115,49 +107,229 @@ def zones():
     
     json_str = json.dumps(manhattan_zones, indent=4, default=str)
     return Response(content=json_str, media_type='application/json')
- 
+
+
 @app.get('/zones/{zone}')
 def get_zone(zone):
-    if isinstance(zone,int):
+    zone = zone.replace('%20',' ')
 
-@app.get('/zones/top-{n}'):
-def top5_zones(n):
-    query = '''
-        SELECT 
+    tabla = 'green_taxis'
+    abr = 'vt'
+    column = 'TotalGreen'
+
+    query = f'''
+    WITH UltimoMes AS (
+    SELECT
+        DATE_SUB(MAX(DATE({abr}.pickup_datetime)), INTERVAL 1 MONTH) AS MesUltimoMes
+    FROM `new_york_transport_project.{tabla}` {abr}
+    )
+
+    SELECT
+    mz.Zone AS Zone,
+    mz.LocationID AS IdZone,
+    (COUNT({abr}.PULocationID) + COUNT({abr}.DOLocationID)) as {column},
+    EXTRACT(MONTH FROM UltimoMes.MesUltimoMes) AS month,
+    EXTRACT(YEAR FROM UltimoMes.MesUltimoMes) AS year
+    FROM `new_york_transport_project.manhattan_zones` mz
+    JOIN `new_york_transport_project.{tabla}` {abr}
+    ON mz.LocationID = {abr}.PULocationID
+    JOIN UltimoMes
+    ON DATE({abr}.pickup_datetime) >= UltimoMes.MesUltimoMes
+    WHERE mz.Zone = '{zone}'
+    GROUP BY Zone, year, month, IdZone;
     '''
+    df1= client.query(query).to_dataframe()
+
+    tabla = 'yellow_taxis'
+    abr = 'yt'
+    column = 'TotalYellow'
+
+    query = f'''
+    WITH UltimoMes AS (
+    SELECT
+        DATE_SUB(MAX(DATE({abr}.pickup_datetime)), INTERVAL 1 MONTH) AS MesUltimoMes
+    FROM `new_york_transport_project.{tabla}` {abr}
+    )
+
+    SELECT
+    mz.Zone AS Zone,
+    mz.LocationID AS IdZone,
+    (COUNT({abr}.PULocationID) + COUNT({abr}.DOLocationID)) as {column},
+    EXTRACT(MONTH FROM UltimoMes.MesUltimoMes) AS month,
+    EXTRACT(YEAR FROM UltimoMes.MesUltimoMes) AS year
+    FROM `new_york_transport_project.manhattan_zones` mz
+    JOIN `new_york_transport_project.{tabla}` {abr}
+    ON mz.LocationID = {abr}.PULocationID
+    JOIN UltimoMes
+    ON DATE({abr}.pickup_datetime) >= UltimoMes.MesUltimoMes
+    WHERE mz.Zone = '{zone}'
+    GROUP BY Zone, year, month, IdZone;
+    '''
+    df2= client.query(query).to_dataframe()
+
+    tabla = 'black_taxis'
+    abr = 'bt'
+    column = 'TotalBlack'
+
+    query = f'''
+    WITH UltimoMes AS (
+    SELECT
+        DATE_SUB(MAX(DATE({abr}.pickup_datetime)), INTERVAL 1 MONTH) AS MesUltimoMes
+    FROM `new_york_transport_project.{tabla}` {abr}
+    )
+
+    SELECT
+    mz.Zone AS Zone,
+    mz.LocationID AS IdZone,
+    (COUNT({abr}.PULocationID) + COUNT({abr}.DOLocationID)) as {column},
+    EXTRACT(MONTH FROM UltimoMes.MesUltimoMes) AS month,
+    EXTRACT(YEAR FROM UltimoMes.MesUltimoMes) AS year
+    FROM `new_york_transport_project.manhattan_zones` mz
+    JOIN `new_york_transport_project.{tabla}` {abr}
+    ON mz.LocationID = {abr}.PULocationID
+    JOIN UltimoMes
+    ON DATE({abr}.pickup_datetime) >= UltimoMes.MesUltimoMes
+    WHERE mz.Zone = '{zone}'
+    GROUP BY Zone, year, month, IdZone;
+    '''
+    df3= client.query(query).to_dataframe()
+
+    tabla = 'grey_taxis'
+    abr = 'gt'
+    column = 'TotalGrey'
+
+    query = f'''
+    WITH UltimoMes AS (
+    SELECT
+        DATE_SUB(MAX(DATE({abr}.pickup_datetime)), INTERVAL 1 MONTH) AS MesUltimoMes
+    FROM `new_york_transport_project.{tabla}` {abr}
+    )
+
+    SELECT
+    mz.Zone AS Zone,
+    mz.LocationID AS IdZone,
+    (COUNT({abr}.PULocationID) + COUNT({abr}.DOLocationID)) as {column},
+    EXTRACT(MONTH FROM UltimoMes.MesUltimoMes) AS month,
+    EXTRACT(YEAR FROM UltimoMes.MesUltimoMes) AS year
+    FROM `new_york_transport_project.manhattan_zones` mz
+    JOIN `new_york_transport_project.{tabla}` {abr}
+    ON mz.LocationID = {abr}.PULocationID
+    JOIN UltimoMes
+    ON DATE({abr}.pickup_datetime) >= UltimoMes.MesUltimoMes
+    WHERE mz.Zone = '{zone}'
+    GROUP BY Zone, year, month, IdZone;
+    '''
+    df4= client.query(query).to_dataframe()
+
+    # Realiza los merges de dos en dos.
+    merged_df1 = pd.merge(df1, df2, on=['Zone','IdZone','month','year'])
+    merged_df2 = pd.merge(df3, df4, on=['Zone','IdZone','month','year'])
+
+    # Luego, combina los resultados anteriores en uno solo.
+    df = pd.merge(merged_df1, merged_df2, on=['Zone','IdZone','month','year'])
+    if len(df) < 1:
+        message = 'No data for this zone in the last month'
+        return message
+    else:
+        df['TotalTrips'] = df.TotalGreen + df.TotalYellow + df.TotalBlack + df.TotalGrey
+        df = df[["Zone",'IdZone','year',"month", "TotalTrips", "TotalGreen", "TotalYellow", "TotalBlack", "TotalGrey"]]
+
+        df['%_Green'] = round((df['TotalGreen'] * 100) / df['TotalTrips'],3)
+        df['%_Yellow'] = round((df['TotalYellow'] * 100) / df['TotalTrips'],3)
+        df['%_Black'] = round((df['TotalBlack'] * 100) / df['TotalTrips'],3)
+        df['%_Grey'] = round((df['TotalGrey'] * 100) / df['TotalTrips'],3)
+
+        data_json = df.to_dict(orient='records')[0]
+        json_str = json.dumps(data_json, indent=4, default=str)
+
+        return Response(content=json_str, media_type='application/json')
 
 
-
-
-# Ruta para la consulta de la cantidad de viajes por categoría
-@app.get('/cantidad_viajes/{categoria}')
-def cantidad_taxis_fecha(categoria):
-
-    table_name = f'{categoria}_taxis'
-
-    # Especifica la referencia a la tabla que deseas visualizar
-    table_ref = client.dataset(dataset_name).table(table_name)
-    table = client.get_table(table_ref)
-
-    query = f"""
-        SELECT COUNT(pickup_datetime)
-        FROM `{project_name}.{dataset_name}.{table_name}`
-        LIMIT 1;
-    """
-
-    query_job = client.query(query)
-    results = query_job.result()
-    for row in results:
-        resultado = row[0]
-
+@app.get('/taxis')
+def home_taxis():
     data_json = {
-        'mensaje': resultado
+        '/greens':'Get a ranking of zones with the highest circulation of green taxis',
+        '/yellows':'Get a ranking of zones with the highest circulation of yellow taxis',
+        '/blacks':'Get a ranking of zones with the highest circulation of black taxis',
+        '/greys':'Get a ranking of zones with the highest circulation of grey taxis'
+
     }
 
     json_str = json.dumps(data_json, indent=4, default=str)
     return Response(content=json_str, media_type='application/json')
 
-# Ruta para la página de inicio
-@app.get('/')
-def home():
-    return {'mensaje': 'Bienvenido a la API para consultar información sobre los taxis de la ciudad de Nueva York.'}
+@app.get('/taxis/{color}')
+def home_taxis(color):
+    if color not in ['greens','yellows','blacks','greys']:
+        data_json = {
+        'taxis/greens':'Get a ranking of zones with the highest circulation of green taxis',
+        'taxis/yellows':'Get a ranking of zones with the highest circulation of yellow taxis',
+        'taxis/blacks':'Get a ranking of zones with the highest circulation of black taxis',
+        'taxis/greys':'Get a ranking of zones with the highest circulation of grey taxis'
+
+        }
+        json_str = json.dumps(data_json, indent=4, default=str)
+
+        return Response(content=json_str, media_type='application/json')
+    else:
+        color = color[:-1]
+        query = f'''
+        WITH UltimoMes AS (
+        SELECT
+            DATE_SUB(MAX(DATE(bt.pickup_datetime)), INTERVAL 1 MONTH) AS FechaUltimoMes
+        FROM `nyc-taxis-project.new_york_transport_project.{color}_taxis` bt
+        )
+
+        SELECT
+        mz.Zone AS Zone,
+        mz.LocationID AS IdZone,
+        EXTRACT(YEAR FROM UltimoMes.FechaUltimoMes) as year,
+        EXTRACT(MONTH FROM UltimoMes.FechaUltimoMes) as month,
+        (COUNT(bt.PULocationID) + COUNT(bt.DOLocationID)) AS TotalTrips
+        FROM `nyc-taxis-project.new_york_transport_project.{color}_taxis` bt
+        JOIN `new_york_transport_project.manhattan_zones` mz
+        ON bt.PULocationID = mz.LocationID
+        JOIN `new_york_transport_project.manhattan_zones` mz2
+        ON bt.DOLocationID = mz2.LocationID
+        JOIN UltimoMes
+        ON DATE(bt.pickup_datetime) >= UltimoMes.FechaUltimoMes
+        GROUP BY Zone, IdZone, year, month
+        ORDER BY TotalTrips DESC;
+        '''
+
+        df = client.query(query).to_dataframe()
+        df['Ranking'] = range(1, len(df) + 1)
+        if color == 'green':
+            df = df[['Ranking','Zone','IdZone','year','month','TotalTrips']].rename(columns={'TotalTrips':'TotalGreenTrips'})
+        elif color == 'yellow':
+            df = df[['Ranking','Zone','IdZone','year','month','TotalTrips']].rename(columns={'TotalTrips':'TotalYellowTrips'})
+        elif color == 'black':
+            df = df[['Ranking','Zone','IdZone','year','month','TotalTrips']].rename(columns={'TotalTrips':'TotalBlackTrips'})
+        elif color == 'grey':
+            df = df[['Ranking','Zone','IdZone','year','month','TotalTrips']].rename(columns={'TotalTrips':'TotalGreyTrips'})
+        
+        data_json = df.to_dict(orient='records')
+        json_str = json.dumps(data_json, indent=4, default=str)
+
+        return Response(content=json_str, media_type='application/json')
+
+@app.get('/zones/top-{n}')
+def top5_zones(n):
+    query = f'''
+    SELECT
+        mz.Zone AS Zone,
+        (COUNT(bt.PULocationID) + COUNT(bt.DOLocationID)) as TotalBlack
+    FROM
+        `new_york_transport_project.manhattan_zones` mz
+    JOIN 
+        `new_york_transport_project.black_taxis` bt
+    ON
+        mz.LocationID = bt.PULocationID
+    GROUP BY Zone
+    ORDER BY TotalBlack DESC
+    LIMIT {n}; 
+    '''
+
+    query_job = client.query(query).to_dataframe()
+    return None
+
